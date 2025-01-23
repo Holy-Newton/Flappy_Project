14754e3fcaf6c03c9f721b3430ae408448fa6df4
François DEBERDT <168488425+Holy-Newton@users.noreply.github.com>
2025-01-23 16:36:57 +0100

    Lot of mistakes but integrations working on it

--- Flappy_one.py
+++ Flappy_one.py
@@ -32,35 +32,81 @@ ORANGE_BIRD = (255, 180, 45)
 WHITE_WING = (255, 230, 200)
 
 
+#----------------------PARAMETERS--------------------------------
+
+SPEED = 10
+GRAVITY = -10
+JUMP = 10
+
+
+#-----------------------PICTURES---------------------------------
+
+bird_image = pygame.image.load('BIRD.png')
+tube_image = pygame.image.load('TUBE.png')
+
+bird_image = pygame.transform.scale(bird_image, (90,90))
+tube_image_fliped = pygame.transform.flip(tube_image, False, True)
 #------------------------MOVES----------------------------------
 
-def on_press():
-    print("la touche espace a été appuyée")
 
-
-
+class move:
+    def __init__(self, x, y, Vx, Vy, Ay, image):
+        self.x = x
+        self.y = y
+        self.Vx = Vx
+        self.Vy = Vy
+        self.Ay = Ay
+        self.image = image
+    
+    def jump(self):
+            if self == bird:
+                self.Vy = JUMP
+            
+    def position_update(self):
+        self.x += self.Vx
+        if self == bird:
+            self.y += self.Vy + GRAVITY/2
+        self.y += self.Vy
+        
+            
+    
 
 def init():
     run = True
     clock = pygame.time.Clock()
     
-
-
-
-    
     while run:
         clock.tick(60)
         WIN.fill(BLUE_SKY)
         
+        #-----------------OBJECTS-------------------------
+        bird = move(WIDTH/2-200, HEIGHT/2 + 20, 0, 0 ,GRAVITY, bird_image)
+        tube_up = move(WIDTH-50, HEIGHT/2 + 20, SPEED, 0 ,0, tube_image)
+        tube_down = move(WIDTH-50, 0, SPEED, 0 ,0, tube_image_fliped)
+      
         
+
+        Object = [bird, tube_up, tube_down]
+
+
+
+        #------------------EVENTS-------------------------
+        
+        for move in Object:
+                move.position_update()
+                
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-
-            elif event.type == pygame.KEYDOWN:
+            if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE:
-                    on_press()
+                    move.Jump(bird)
+            
+
                 
+        #WIN.blit(bird_image, ((WIDTH/2-200), HEIGHT/2))
+        #WIN.blit(Tube_image, ((WIDTH/2), HEIGHT/2))
+        #WIN.blit(Tube_image_fliped, ((WIDTH/2), 0))  
         pygame.display.update()
     pygame.quit()
 
