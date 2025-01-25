d26885c55a9d4d6223638b6e8749211b4e15aa4a
François DEBERDT <168488425+Holy-Newton@users.noreply.github.com>
2025-01-25 13:41:03 +0100

    Version fonctionnelle sans obstacles

--- Flappy_one.py
+++ Flappy_one.py
@@ -1,13 +1,13 @@
 import pygame
 import math as m
 from pynput import keyboard
-
+import random
 pygame.init()
 
 WIDTH, HEIGHT = 1200,800
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 
-pygame.display.set_caption("HOLLY_HOLLY_HOLLY_HOLLY------ FLAPPY-BIRD by Holly-Newton -----HOLLY_HOLLY_HOLLY_HOLLY")
+pygame.display.set_caption("HOLY_HOLY_HOLY_HOLY_HOLY_HOLY_HOLY------ FLAPPY-BIRD by Holy-Newton -----HOLY_HOLY_HOLY_HOLY_HOLY_HOLY_HOLY")
 
 #------------------------COLORS----------------------------------
 
@@ -21,6 +21,8 @@ SATURN_COLOR = (210, 180, 140)
 JUPITER_COLOR = (200, 100, 50)
 SPACE_GREY_BLUE = (10, 12, 40)
 
+FONT_TITLE = pygame.font.SysFont("marker felt",40)
+FONT_TITLE_POINT = pygame.font.SysFont("marker felt",80)
 
 BLUE_SKY = (140, 190, 255)
 GROUND = (222, 186, 136)
@@ -32,42 +34,70 @@ WHITE_WING = (255, 230, 200)
 
 
 #----------------------PARAMETERS--------------------------------
-
+FLY = 10.5
 SPEED = 10
-GRAVITY = 8
+GRAVITY = 0.8
 TIME_STEP = 1
 y = HEIGHT/2
 Vy = 0
+Tx = WIDTH
+T2x = WIDTH
+alea = 2
+alea2 = 3
+decount = 70
+point = 0
 #-----------------------PICTURES---------------------------------
-
+cloud_image = pygame.image.load('CLOUD.png')
 bird_image = pygame.image.load('BIRD.png')
 tube_image = pygame.image.load('TUBE.png')
 
-bird_image = pygame.transform.scale(bird_image, (90,90))
+bird_image = pygame.transform.scale(bird_image, (70,50))
+tube_image = pygame.transform.scale2x(tube_image)
 tube_image_fliped = pygame.transform.flip(tube_image, False, True)
+tube_image2 = tube_image
+tube_image_fliped2 = tube_image_fliped
 
-#----------------------BIRD_MOVMEMENT----------------------------      
+
+#-------------------------MOVMEMENTS------------------------------    
             
+            
+def Tube_mv(Tx, H):
+    WIN.blit(tube_image_fliped, (Tx, 50-H*60))
+    WIN.blit(tube_image, (Tx, WIDTH/1.5-H*60))
+    
+def Tube2_mv(Tx, H):
+    WIN.blit(tube_image_fliped2, (Tx, 50-H*60))
+    WIN.blit(tube_image2, (Tx, WIDTH/1.5-H*60))
+
 
 #-----------------------------------------------------------------
 def init():
     global y
     global Vy
+    global Tx
+    global T2x
+    global alea
+    global alea2
+    global decount
+    global point
     run = True
     clock = pygame.time.Clock()
-
+    
     
     JUMP = False
 
     while run:
         clock.tick(60)
         WIN.fill(BLUE_SKY)
-
-
+        WIN.blit(cloud_image, (9,0))
+        Intern_title=FONT_TITLE.render("FLAPPY BIRD by Holy_Newton", 1, YELLOW_BIRD)
+        WIN.blit((Intern_title), (100,100)) 
         
         #------------------EVENTS-------------------------
-        
-    
+        if Tx == WIDTH/2-200:
+            point += 1
+        if T2x == WIDTH/2-200:
+            point += 1
                 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -75,18 +105,47 @@ def init():
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE:
                     JUMP = True
+                   
 
+        ### BIRD MANIPULATION ###
         if JUMP:
-            Vy = -10
+            Vy = -FLY
         else:
-            Vy = Vy
+            Vy = Vy+ GRAVITY
         JUMP = False
-        y = y + Vy + GRAVITY
+        y = y + Vy
+
+        if y > HEIGHT - bird_image.get_height():
+            y = HEIGHT - bird_image.get_height()
+        elif y < 0:
+            y = 0
+
         WIN.blit(bird_image, ((WIDTH/2-200),y))
-        WIN.blit(tube_image, ((WIDTH/2), HEIGHT/2))
-        WIN.blit(tube_image_fliped, ((WIDTH/2), 0))  
+
+        ### TUBE MANIPULATION ###
+
+        Tx += -SPEED
+        if Tx > -100:
+            Tube_mv(Tx,alea)
+        else:
+            alea = random.randint(1,9)
+            Tx = WIDTH+100
+
+        if decount<0:
+            T2x += -SPEED
+            if T2x > -100:
+                Tube2_mv(T2x,alea2)
+            else:
+                alea2 = random.randint(1,9)
+                T2x = WIDTH+100
+
+        decount -= 1
+
+        POINT=FONT_TITLE_POINT.render(f"{point}", 1, WHITE)
+        WIN.blit((POINT), (WIDTH - 150,100))
+
         pygame.display.update()
-    pygame.quit()
+    pygame.quit() 
 
 
 init()
